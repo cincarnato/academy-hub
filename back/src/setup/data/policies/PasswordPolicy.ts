acaefb57951ba9f21723a00a5fe1890429f27ec8
Cristian Incarnato <cristian.cdi@gmail.com>
2026-04-26 11:49:46 -0300

    Initial commit

---
+++ back/src/setup/data/policies/PasswordPolicy.ts
@@ -0,0 +1,17 @@
+import type {IPasswordPolicy} from "@drax/identity-share";
+
+const projectPasswordPolicy: IPasswordPolicy = {
+    minLength: 8,
+    maxLength: 32,
+    requireUppercase: true,
+    requireLowercase: true,
+    requireNumber: true,
+    requireSpecialChar: true,
+    disallowSpaces: false,
+    preventReuse: 3,
+    expirationDays: null
+}
+
+export {
+    projectPasswordPolicy
+}
